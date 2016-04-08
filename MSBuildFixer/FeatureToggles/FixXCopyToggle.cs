f8c0d443a31d44a29979dd27c1a1eb09d10a592c
KipV <rvorderm@gmail.com>
2016-04-08 10:27:43 -0600

    Fixing broken merge
    Whoops

---
+++ MSBuildFixer/FeatureToggles/FixXCopyToggle.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using FeatureToggle.Toggles;
+
+namespace MSBuildFixer.FeatureToggles
+{
+	public class FixXCopyToggle : SimpleFeatureToggle
+	{
+		private static readonly Lazy<FixXCopyToggle> Lazy =
+		new Lazy<FixXCopyToggle>(() => new FixXCopyToggle());
+
+		public static FixXCopyToggle Instance => Lazy.Value;
+
+		public static bool Enabled => Instance.FeatureEnabled;
+	}
+}
