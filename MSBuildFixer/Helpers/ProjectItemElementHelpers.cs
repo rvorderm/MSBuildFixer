3eea4d2e5d6d957857a659953027a1162c4e4756
Ryan Vordermann <vord1204@nmsu.edu>
2017-01-19 15:08:58 -0700

    Added some helper functions for assembly name and version info.

---
+++ MSBuildFixer/Helpers/ProjectItemElementHelpers.cs
@@ -0,0 +1,21 @@
+﻿namespace MSBuildFixer.Helpers
+{
+	public static class ProjectItemElementHelpers
+	{
+		public static string GetAssemblyName(string include)
+		{
+			int firstComma = include.IndexOf(",");
+			return firstComma == -1 ? include : include.Substring(0,firstComma);
+		}
+
+		public static string GetVersion(string include)
+		{
+			int firstComma = include.IndexOf(",");
+			int secondComma = include.IndexOf(",", firstComma+1);
+			int version = include.IndexOf("Version=");
+			int versionLength = "Version=".Length;
+			//Console.Out.Write($"{firstComma}, {secondComma}, {version}, {versionLength}");
+			return firstComma == -1 ? null : include.Substring(version+versionLength, secondComma-(versionLength+version));
+		}
+	}
+}
---
+++ MSBuildFixerTests/Helpers/ProjectItemElementHelpersTests.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using MSBuildFixer.Helpers;
+
+namespace MSBuildFixerTests.Helpers
+{
+	[TestClass]
+	public class ProjectItemElementHelpersTests
+	{
+		[TestMethod]
+		public void GetAssemblyFromWeakNameString()
+		{
+			string assemblyName = ProjectItemElementHelpers.GetAssemblyName("FakeItEasy");
+			Assert.AreEqual("FakeItEasy", assemblyName);
+		}
+
+		[TestMethod]
+		public void GetAssemblyFromStrongNameString()
+		{
+			string assemblyName = ProjectItemElementHelpers.GetAssemblyName("FakeItEasy, Version=2.3.0.0, Culture=neutral, PublicKeyToken=eff28e2146d5fd2c, processorArchitecture=MSIL");
+			Assert.AreEqual("FakeItEasy", assemblyName);
+		}
+
+		[TestMethod]
+		public void GetVersionFromWeakNameString()
+		{
+			string assemblyName = ProjectItemElementHelpers.GetVersion("FakeItEasy");
+			Assert.AreEqual(null, assemblyName);
+		}
+
+		[TestMethod]
+		public void GetVersionFromStrongNameString()
+		{
+			string assemblyName = ProjectItemElementHelpers.GetVersion("FakeItEasy, Version=2.3.0.0, Culture=neutral, PublicKeyToken=eff28e2146d5fd2c, processorArchitecture=MSIL");
+			Assert.AreEqual("2.3.0.0", assemblyName);
+		}
+	}
+}
