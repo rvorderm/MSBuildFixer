03aa6883bebc15c168d8bd8ab212c3e19ae1f93a
Ryan Vordermann <vord1204@nmsu.edu>
2017-01-18 17:37:58 -0700

    I added the ability to excluded specific projects.

---
+++ MSBuildFixer/Configuration/ExclusionConfiguration.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.IO;
+using System.Linq;
+
+namespace MSBuildFixer.Configuration
+{
+	public class ExclusionConfiguration
+	{
+		public IEnumerable<string> Projects { get; set; }
+
+
+		private static ExclusionConfiguration _exclustionConfiguration;
+
+
+		public static bool IsExcludedProject(string absolutePath)
+		{
+			string fileName = Path.GetFileName(absolutePath);
+			if(string.IsNullOrEmpty(fileName)) throw new ArgumentException($"{nameof(absolutePath)} must be a valid file name");
+			_exclustionConfiguration = (dynamic) ConfigurationManager.GetSection("exclusionConfiguration");
+			return _exclustionConfiguration.Projects?.Any(exclusion => fileName.Equals(exclusion)) ?? false;
+		}
+	}
+}
