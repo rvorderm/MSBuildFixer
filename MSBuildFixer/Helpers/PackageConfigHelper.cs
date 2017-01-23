3bb3b07139240490096d05c0bee92828fb29c290
Ryan Vordermann <vord1204@nmsu.edu>
2017-01-23 13:51:50 -0700

    I rewrote the PAckageConfigHelper to queue up a list of actions instead of operating immediately. That way I don't build  a file unless it needs to be build.

--- MSBuildFixer/Helpers/PackageConfigHelper.cs
+++ MSBuildFixer/Helpers/PackageConfigHelper.cs
@@ -1,4 +1,6 @@
-﻿using System.IO;
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Xml;
 
@@ -7,70 +9,94 @@ namespace MSBuildFixer.Helpers
 	public class PackageConfigHelper
 	{
 		private readonly string _packageFilePath;
-		private readonly XmlDocument _xmlDocument = new XmlDocument();
+		private XmlDocument _xmlDocument { get; set; }
+		private List<Action> Actions { get; set; } = new List<Action>();
 
 		public PackageConfigHelper(string packageFilePath)
 		{
 			_packageFilePath = packageFilePath;
-			if (!File.Exists(packageFilePath))
-			{
-				File.WriteAllText(packageFilePath, "<?xml version=\"1.0\" encoding=\"utf-8\"?>\r\n<packages>\r\n</packages>");
-			}
-			_xmlDocument.Load(packageFilePath);
 		}
 
 		public PackageConfigHelper AddOrUpdate(string assemblyName, string version)
 		{
-			XmlElement xmlElement = GetElement(_xmlDocument, assemblyName);
+			Actions.Add(()=>AddOrUpdate_Action(assemblyName, version));
+			return this;
+		}
+
+		public PackageConfigHelper Update(string assemblyName, string version)
+		{
+			Actions.Add(()=>Update_Action(assemblyName, version));
+			return this;
+		}
+
+		public void SavePackageFile()
+		{
+			if (!Actions.Any()) return;
+			CreateAndLoadFile();
+			foreach (Action action in Actions)
+			{
+				action.Invoke();
+			}
+			_xmlDocument.Save(_packageFilePath);
+		}
+
+		private XmlElement GetElement(string id)
+		{
+			XmlNodeList xmlNodeList = _xmlDocument.SelectNodes("//package");
+			return xmlNodeList?.Cast<XmlElement>().FirstOrDefault(elem => elem.Attributes["id"].Value.Equals(id));
+		}
+
+		private XmlElement AddPackage(string id, string version)
+		{
+			XmlElement child = _xmlDocument.CreateElement("package");
+			addAttribute(child, "id", id);
+			addAttribute(child, "version", version);
+			addAttribute(child, "targetFramework", "net461");
+			return child;
+		}
+
+		private void addAttribute(XmlNode elem, string name, string version)
+		{
+			XmlAttribute xmlAttribute = _xmlDocument.CreateAttribute(name);
+			xmlAttribute.Value = version;
+			elem.Attributes?.Append(xmlAttribute);
+		}
+
+		private void CreateAndLoadFile()
+		{
+			if (!File.Exists(_packageFilePath))
+			{
+				File.WriteAllText(_packageFilePath, "<?xml version=\"1.0\" encoding=\"utf-8\"?>\r\n<packages>\r\n</packages>");
+			}
+			if (_xmlDocument == null)
+			{
+				_xmlDocument = new XmlDocument();
+				_xmlDocument.Load(_packageFilePath);
+			}
+		}
+
+		private void AddOrUpdate_Action(string assemblyName, string version)
+		{
+			XmlElement xmlElement = GetElement(assemblyName);
 			if (xmlElement == null)
 			{
 				xmlElement = (XmlElement)_xmlDocument.SelectSingleNode("//packages");
-				XmlElement child = AddPackage(_xmlDocument, assemblyName, version);
+				XmlElement child = AddPackage(assemblyName, version);
 				xmlElement?.AppendChild(child);
 			}
 			else
 			{
 				xmlElement.Attributes["version"].Value = version;
 			}
-			return this;
 		}
 
-		public PackageConfigHelper Update(string assemblyName, string version)
+		private void Update_Action(string assemblyName, string version)
 		{
-			XmlElement xmlElement = GetElement(_xmlDocument, assemblyName);
+			XmlElement xmlElement = GetElement(assemblyName);
 			if (xmlElement != null)
 			{
 				xmlElement.Attributes["version"].Value = version;
 			}
-			return this;
-		}
-
-		private static XmlElement GetElement(XmlNode xmlDocument, string id)
-		{
-			XmlNodeList xmlNodeList = xmlDocument.SelectNodes("//package");
-			return xmlNodeList?.Cast<XmlElement>().FirstOrDefault(elem => elem.Attributes["id"].Value.Equals(id));
-		}
-
-		private static XmlElement AddPackage(XmlDocument xmlDocument, string id, string version)
-		{
-			XmlElement child = xmlDocument.CreateElement("package");
-			addAttribute(xmlDocument, child, "id", id);
-			addAttribute(xmlDocument, child, "version", version);
-			addAttribute(xmlDocument, child, "targetFramework", "net461");
-			return child;
-		}
-
-		private static void addAttribute(XmlDocument xmlDocument, XmlNode elem, string name, string version)
-		{
-			XmlAttribute xmlAttribute = xmlDocument.CreateAttribute(name);
-			xmlAttribute.Value = version;
-			elem.Attributes?.Append(xmlAttribute);
-		}
-
-		public void SavePackageFile()
-		{
-			if (_xmlDocument.SelectNodes("//package")?.Count <= 0) return;
-			_xmlDocument.Save(_packageFilePath);
 		}
 	}
 }
