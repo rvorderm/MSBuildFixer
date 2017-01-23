f5a355c92c1a635301a2ac4584633f37a1918d33
Ryan Vordermann <vord1204@nmsu.edu>
2017-01-23 14:00:04 -0700

    Added a fixes configuration file to begin moving away from the FeatureToggles. While easy to use, FeatureToggles aren't flexible enough for some of the inputs anymore.

---
+++ MSBuildFixer/Configuration/FixesConfiguration.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections.Generic;
+using System.Configuration;
+
+namespace MSBuildFixer.Configuration
+{
+	public class FixesConfiguration
+	{
+		public static FixesConfiguration Instance = (dynamic)ConfigurationManager.GetSection("FixesConfiguration");
+		public IEnumerable<ProjectReferenceReplacement> ProjectReferenceReplacements { get; set; }
+	}
+
+	public class ProjectReferenceReplacement
+	{
+		public string Name { get; set; }
+		public string Replacement { get; set; }
+	}
+}
