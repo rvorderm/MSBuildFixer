37e70f997b897ac0e0a3f4228454a81d581d07e2
Ryan Vordermann <vord1204@nmsu.edu>
2017-01-23 13:54:20 -0700

    Factored out a couple useful methods from other fixes.

--- MSBuildFixer/Helpers/ProjectRootElementHelpers.cs
+++ MSBuildFixer/Helpers/ProjectRootElementHelpers.cs
@@ -1,6 +1,7 @@
 ﻿// ReSharper disable StringIndexOfIsCultureSpecific.1
 // ReSharper disable StringIndexOfIsCultureSpecific.2
 
+using System.IO;
 using Microsoft.Build.Construction;
 using System.Linq;
 
@@ -11,6 +12,19 @@ namespace MSBuildFixer.Helpers
 		public static string GetAssemblyName(ProjectRootElement rootElement)
 		{
 			return rootElement.Properties.FirstOrDefault(x=>x.Name.Equals("Assembly"))?.Value;
+		}
+
+		public static void AddProjectReference(ProjectRootElement projectRootElement, ProjectInSolution project)
+		{
+			ProjectItemElement itemElement = projectRootElement.AddItem("ProjectReference", project.RelativePath);
+			itemElement.AddMetadata("Project", project.ProjectGuid);
+		}
+
+		public static string GetNugetPackagePath(ProjectRootElement rootElement)
+		{
+			string directoryName = Path.GetDirectoryName(rootElement.FullPath);
+			string packagePath = Path.Combine(directoryName, "packages.config");
+			return packagePath;
 		}
 	}
 }
